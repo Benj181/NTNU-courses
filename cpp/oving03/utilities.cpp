60dc0058f9bcf3c8e4d862dd04c896cd1bed1b09
Benjamin Aas <battlebeast.overwatch@gmail.com>
2025-01-30 09:55:38 +0100

    Finished Excercise 3

--- cpp/oving03/utilities.cpp
+++ cpp/oving03/utilities.cpp
@@ -1,6 +1,11 @@
 #include "utilities.h"
-#include <random>
 
 // BEGIN: 5a
+int randomWithLimits(int lower, int upper){
+    std::random_device rd;
+    std::default_random_engine generator(rd());
+    std::uniform_int_distribution<int> distribution(lower, upper);
+    return distribution(generator);
 
+}
 // END: 5a
