77cbd6e18249e6c37890bebec286b2e600b9a50b
Benjamin Aas <battlebeast.overwatch@gmail.com>
2025-02-04 09:31:46 +0100

    Init of oving05
    Used handout file, not extention to import

---
+++ cpp/oving05/Card.cpp
@@ -0,0 +1,25 @@
+#include "Card.h"
+
+// BEGIN: 1c
+
+// END: 1c
+
+// BEGIN: 1d
+
+// END: 1d
+
+// BEGIN: 2b
+
+// END: 2b
+
+// BEGIN: 2c
+
+// END: 2c
+
+// BEGIN: 2d
+
+// END: 2d
+
+// BEGIN: 2e
+
+// END: 2e
