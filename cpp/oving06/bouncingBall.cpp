1d3b6af4c9f7d2d3c866505e73b1b688dc3663a5
Benjamin Aas <battlebeast.overwatch@gmail.com>
2025-02-13 08:40:32 +0100

    done ov6-opg4d

--- cpp/oving06/bouncingBall.cpp
+++ cpp/oving06/bouncingBall.cpp
@@ -8,7 +8,7 @@ map<int, Color> ball_color = {
     {3, Color::red},
     {4, Color::green}    
 };
-
+ 
 // END: 4b
 
 // BEGIN: 4c
@@ -39,16 +39,15 @@ void bouncingBall(){
     Color color_down;
     Config slow;
     Config fast;
-    const int radius{30};
+    const int radius{10};
     int alpha{1};
     int velocity{2};
     int x{0};
     int y{360};
     int increment_x{0};
     int increment_y{0};
-    int count_bounce_top{0};
-    int count_bounce_bottom{0};
     int count_num_passes{0};
+    bool moving_down{true};
 
 
     // read in the configurations
@@ -67,7 +66,7 @@ void bouncingBall(){
         increment_y = velocity * sin(alpha);
 
         // movement i x-direction
-        if ((increment_x + x) > window.width()) {
+        if ((increment_x + x) + radius> window.width()) {
             // reached right side - wrap around to the leftmost
             x = 0;
             // increment counter which counts number of full left-to-right passes
@@ -89,19 +88,33 @@ void bouncingBall(){
             x += increment_x;
         }
 
-
+        
         // HINT 1: Ta en titt på koden ovenfor og se hvilke variabler som brukes. 
         // Bruk disse variablene for å implementere bevegelsen til den sprettende ballen. For eksempel:
         // Variablene count_bounce_top og count_bounce_bottom kan brukes
         // for å holde styr på hvilken retning ballen skal gå. 
         // Variabelen increment_y skal brukes for å øke y.
-
+        
         // HINT 2: window.draw_circle({x, y}, r, c) tegner en sirkel med radius r, 
         // sentrum i (x, y) og fargen c
-
+        
         // BEGIN: 4d
         // movement in y-direction
-        window.draw_circle({x, y}, radius, color_up);
+        if (moving_down){
+            if ((increment_y + y) + radius > window.height()) {
+                moving_down = false;
+            } else {
+                y += increment_y;
+                window.draw_circle({x, y}, radius, color_down);
+            }
+        } else {
+            if ((increment_y + y) - radius < 0) {
+                moving_down = true;
+            } else {
+                y -= increment_y;
+                window.draw_circle({x, y}, radius, color_up);
+            }
+        }
         // END: 4d
     
         window.next_frame();
