f5a35b0ac5e9bd6d85e6d6c9d6c64588e5abc3b3
Benjamin Aas <battlebeast.overwatch@gmail.com>
2025-03-03 09:42:32 +0100

    init

---
+++ cpp/oving09/measurePerformance.cpp
@@ -0,0 +1,9 @@
+#include "Stopwatch.h"
+#include <memory>
+#include <array>
+#include <iostream>
+
+
+// BEGIN 1a
+
+// END 1a
---
+++ cpp/oving09/optimizeVector.cpp
@@ -0,0 +1,20 @@
+#include "Stopwatch.h"
+#include "optimizeVector.h"
+#include <vector>
+#include <iostream>
+
+
+// BEGIN 2a
+
+// END 2a
+
+
+void optimizeVector(){
+    // BEGIN 2d
+    
+    // END 2d
+
+    // BEGIN 2e
+    
+    // END 2e   
+}
