4b71740e32333e5535e7d405fc08dbe1d3aedcc1
Benjamin Aas <102666307+Benj181@users.noreply.github.com>
2025-03-12 14:47:58 +0100

    small changes

--- Optikk/Lab02/main.py
+++ Optikk/Lab02/main.py
@@ -25,7 +25,17 @@ for i, s_1 in enumerate(s_1s):
     ls[i] = l  
 
 print(ls.min())
-print(calc(150))
+s_1 = 150
+print('s_1:', s_1)
+M_, s_2, s_i1, s_o2, s_i2, l = calc(150)
+print('s_i1:', s_i1)
+print('s_o2:', s_o2)
+print('s_i2:', s_i2)
+print('Position of lense 1:', s_1)
+print('Position of lense 2:', s_2)
+print('Length of system', l)
+print('M:', M_)
+
 
 for i, s_1 in enumerate(s_1s):
     M_, s_2, s_i1, s_o2, s_i2, l = calc(s_1)
