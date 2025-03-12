9ade8d105e86be2bb1a459fdb733b9a67103468d
Benjamin Aas <102666307+Benj181@users.noreply.github.com>
2025-03-12 14:35:40 +0100

    done

--- Optikk/Lab02/main.py
+++ Optikk/Lab02/main.py
@@ -1,32 +1,38 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-f = [25.4, 100, 200]
 M = 6
-
-f_1 = f[0]
-f_2 = f[1]
-
-f_1 = 25.4
+f_1 = 100
 f_2 = 100
 
-x = lambda s_1: s_1/f_1-1
-y = lambda x: 1/(M*x)
-s_2 = lambda y, s_1: f_2*(y+1) + s_1 + (1/f_1-1/s_1)**-1
 
-#--------------------------------------------------------#
+def calc(s_1):
+    x = s_1 / f_1 - 1
+    y = 1 / (M * x)
+    s_o2 = f_2*(1 + y)
+    s_2 = s_o2 + s_1 + (1/f_1-1/s_1)**-1
+    s_i1 = (1/f_1-1/s_1)**-1
+    s_i2 = (1/f_2-1/s_o2)**-1
+    l = s_2 + s_i2
+    return M, s_2, s_i1, s_o2, s_i2, l
 
-s_1s = np.linspace(30, 150, 100)
-s_2s = []
-for s_1 in s_1s:
-    x_ = x(s_1)
-    y_ = y(x_)
-    s_2s.append(s_2(y_, s_1))
+n = 1000
+ls = np.zeros(n)
+s_1s = np.linspace(110, 300, n)
 
-plt.plot(s_1s, s_2s, label='f_1 = 25.4, f_2 = 100')
+for i, s_1 in enumerate(s_1s):
+    M_, s_2, s_i1, s_o2, s_i2, l = calc(s_1)
+    ls[i] = l  
+
+print(ls.min())
+print(calc(150))
+
+for i, s_1 in enumerate(s_1s):
+    M_, s_2, s_i1, s_o2, s_i2, l = calc(s_1)
+    plt.plot(s_1, l, 'ro', markersize=1)
+
 plt.xlabel('s_1')
-plt.ylabel('s_2')
-plt.grid()
-plt.legend()
+plt.ylabel('l')
 plt.show()
+
 
